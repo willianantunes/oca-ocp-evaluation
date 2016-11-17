1965f615cb126d77676f3454d27f28f19ee5c3e6
Willian Antunes <willian.lima.antunes@gmail.com>
2016-11-17 10:26:56 -0200

    Better understanding

--- src/main/java/br/com/willianantunes/examocp/MergeExampleMaps.java
+++ src/main/java/br/com/willianantunes/examocp/MergeExampleMaps.java
@@ -7,7 +7,10 @@ import java.util.function.BiFunction;
 public class MergeExampleMaps {
 	public static void main(String[] args) {
 		BiFunction<String, String, String> mapper = (v1, v2) -> { 
-			return v1.length() >= v2.length()? v1 : v2;
+			boolean result = v1.length() >= v2.length();
+			System.out.println(String.format("v1: %s / v2: %s ==> Result: %s", v1, v2, result));
+			// Like "Do I must keep the original one?
+			return result? v1 : v2;
 		};
 		
 		Map<String, String> favorites = new HashMap<>();
@@ -19,5 +22,20 @@ public class MergeExampleMaps {
 		System.out.println(favorites); // {Tom=Tram, Jenny=Skyrideee}
 		String tom = favorites.merge("Tom", "Skyride", mapper);
 		System.out.println(favorites); // {Tom=Skyride, Jenny=Skyrideee}
+		String antunes = favorites.merge("Antunes", "HWorld", mapper);
+		System.out.println(favorites); // {Tom=Skyride, Jenny=Skyrideee, Antunes=HWorld}
+		String antunes2 = favorites.merge("Antunes", "HWor", mapper);
+		System.out.println(favorites); // {Tom=Skyride, Jenny=Skyrideee, Antunes=HWorld}
+		
+		/*
+		{Tom=Tram, Jenny=Bus tour}
+		v1: Bus tour / v2: Skyrideee ==> Result: false
+		{Tom=Tram, Jenny=Skyrideee}
+		v1: Tram / v2: Skyride ==> Result: false
+		{Tom=Skyride, Jenny=Skyrideee}
+		{Tom=Skyride, Jenny=Skyrideee, Antunes=HWorld}
+		v1: HWorld / v2: HWor ==> Result: true
+		{Tom=Skyride, Jenny=Skyrideee, Antunes=HWorld}
+		*/
 	}
 }
