b5051a09e9b37fb86bb35bb563c5a6b70d75c685
Willian Antunes <willian.lima.antunes@gmail.com>
2016-12-03 20:37:54 -0200

    Recursion with join/fork framework

--- src/main/java/br/com/willianantunes/examocp/chap7/LionPenManager.java
+++ src/main/java/br/com/willianantunes/examocp/chap7/LionPenManager.java
@@ -1,4 +1,4 @@
-package br.com.willianantunes.examocp.chap7.parallelStream;
+package br.com.willianantunes.examocp.chap7;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
---
+++ src/main/java/br/com/willianantunes/examocp/chap7/WeighAnimalAction.java
@@ -0,0 +1,50 @@
+package br.com.willianantunes.examocp.chap7;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * See page 382.
+ */
+public class WeighAnimalAction extends RecursiveAction {
+
+	private int start;
+	private int end;
+	private Double[] weights;
+	
+	public WeighAnimalAction(Double [] weights, int start, int end) {
+		this.weights = weights;
+		this.start = start;
+		this.end = end;
+	}
+	
+	@Override
+	protected void compute() {
+		if (end-start <= 3) { // OUR BASE CASE
+			for(int i=start; i<end; i++) {
+				weights[i] = (double) new Random().nextInt(100);
+				System.out.println("Animal weighed: " + i);
+			}
+		} else { // OUR RECURSIVE CASE
+			int middle = start + ((end-start)/2);
+			System.out.println("[start="+start+",middle="+middle+",end="+end+"]");
+			invokeAll(new WeighAnimalAction(weights, start, middle), new WeighAnimalAction(weights, middle, end));
+		}
+	}
+
+	public static void main(String args[]) {
+		Double[] weights = new Double[10];
+		ForkJoinTask<?> task = new WeighAnimalAction(weights, 0, weights.length);
+		ForkJoinPool pool = new ForkJoinPool();
+		pool.invoke(task);
+		
+		// Print results
+		System.out.println(); System.out.println();
+		System.out.print("Weights: ");
+		Arrays.asList(weights).stream().forEach(d -> System.out.print(d.intValue() + " "));
+		
+	}
+}
